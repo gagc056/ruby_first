6621e114c7b2c4b8581c470c0c8f97fdedbee0aa
lhscar <gagc056@gmail.com>
2019-10-07 23:06:58 +0200

    changing the ruby file

--- buble_sort.rb
+++ buble_sort.rb
@@ -1,8 +1,11 @@
 def buble_sort(arr)
     n=array.lenght
-    loop do
-        sorted=false
-        (n-1).times do|i|
+    sorted = -1
+    while sorted != 0
+        swap = 0
+        loop do
+          sorted=false
+          (n-1).times do|i|
             if arr[i]>arr[i+1]
                 arr[i],arr[i+1]=arr[i+1],arr[i]
                 sorted=true
@@ -11,8 +14,14 @@ def buble_sort(arr)
         end
         break if not sorted
     end
-    arr
+    output = arr.join(", ")
+    return "[#{output}]"
 end
+
+
+arr = gets.chomp.split(" ")
+arr = arr.map {|num| num.to_i}
+puts bubble_sort(arr)
 
 x=[4,3,78,2,0,2]
 puts buble_sort(x)
