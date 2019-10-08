3ca18a83604ed4a8979335e990efbd90d1a474dd
juanpablogil <jpablomgil@gmail.com>
2019-10-08 14:43:02 -0500

    Second part complete

--- buble_sort_by.rb
+++ buble_sort_by.rb
@@ -1,17 +1,21 @@
-    def bubble_sort(array)
-        if block_given?
-                (array.length-1).times do |i|
-                    i=0
-                    if  yield(array[i] ,array[i+1])>0
-                        array[i], array[i+1] = array[i+1], array[i]
-                    end
-                    i+=1
-                end
-        else
-        return array
-        end
-    end
+def bubble_sort_by(array)
+  s = false
 
-    bubble_sort_by(["hi","hello","hey"]) do |left,right|
-        left.length - right.length
+  while s == false
+    s = true
+
+    (array.length-1).times do |i|
+      if yield(array[i], array[i+1]) > 0
+        array[i], array[i+1] = array[i+1], array[i]
+        s = false
+      end
     end
+  end
+
+  array
+
+end
+
+x = ["abcd","ab","abcde","abc","a"]
+
+puts bubble_sort_by(x) { |left,right| left.length - right.length }
