9a976cd537022e71d5a436ba7b521e19afc57ce1
lhscar <gagc056@gmail.com>
2019-10-08 19:00:56 +0200

    fixing buble_sort_by file

--- buble_sort_by.rb
+++ buble_sort_by.rb
@@ -1,15 +1,12 @@
     def bubble_sort(array)
         if block_given?
-            sorted = false
-            loop do
                 (array.length-1).times do |i|
+                    i=0
                     if  yield(array[i] ,array[i+1])>0
                         array[i], array[i+1] = array[i+1], array[i]
-                        sorted = true
                     end
+                    i+=1
                 end
-                break if not sorted
-            end
         else
         return array
         end
