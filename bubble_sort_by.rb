a8db55521f26fa6caa497a84e60bb3eaccd2566b
lhscar <gagc056@gmail.com>
2019-10-11 21:13:40 +0200

    fixing rubocop file

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,5 +1,4 @@
 def bubble_sort_by(array)
-<<<<<<< HEAD
   if block_given?
           (array.length-1).times do |i|
               i=0
@@ -11,24 +10,7 @@ def bubble_sort_by(array)
   else
   array
   end
-=======
-  s = false
-
-  while s == false
-    s = true
-
-    (array.length-1).times do |i|
-      if yield(array[i], array[i+1]) > 0
-        array[i], array[i+1] = array[i+1], array[i]
-        s = false
-      end
-    end
-  end
-  array
->>>>>>> fe68c212317829ab4dc9646333a30b8562a86d77
 end
 
-
 x = ["abcd","ab","abcde","abc","a"]
-
 puts bubble_sort_by(x) { |left,right| left.length - right.length }
