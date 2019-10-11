f2bf955985bd8296d5c4f2b4aa99327e593b00d0
lhscar <gagc056@gmail.com>
2019-10-11 21:35:32 +0200

    fixing sinta errors

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,14 +2,14 @@ def bubble_sort_by(array)
   if block_given?
     (array.length-1).times do |i|
       i=0
-      if yield(array[i], array[i+1])>0
-        array[i], array[i+1] = array[i+1], array[i]
+      if yield(array[i], array[i + 1]).positive
+        array[i], array[i + 1] = array[i + 1], array[i]
       end
       i+=1
     end
   else
-  array
+    array
   end
 end
-x = ["abcd","ab","abcde","abc","a"]
+x = ["abcd", "ab", "abcde", "abc", "a"]
 puts bubble_sort_by(x) { |left,right| left.length - right.length }
