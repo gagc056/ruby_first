d21381cf322dc736d2ff21fcc273078ba1e20d26
lhscar <gagc056@gmail.com>
2019-10-11 21:07:23 +0200

    fixing stickler

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,28 +1,17 @@
-    def bubble_sort_by(array)
-        if block_given?
-                (array.length-1).times do |i|
-                    i=0
-                    if  yield(array[i] ,array[i+1])>0
-                        array[i], array[i+1] = array[i+1], array[i]
-                    end
-                    i+=1
-                end
-        else
-        return array
-        end
-    end
-
-    (array.length-1).times do |i|
-      if yield(array[i], array[i+1]) > 0
-        array[i], array[i+1] = array[i+1], array[i]
-        s = false
-      end
-    end
-  end
-
+def bubble_sort_by(array)
+  if block_given?
+          (array.length-1).times do |i|
+              i=0
+              if  yield(array[i] ,array[i+1])>0
+                  array[i], array[i+1] = array[i+1], array[i]
+              end
+              i+=1
+          end
+  else
   array
-
+  end
 end
+
 
 x = ["abcd","ab","abcde","abc","a"]
 
