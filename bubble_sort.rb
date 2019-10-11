71171ddde6f8099575ad1ed4010dacfc24121db5
lhscar <gagc056@gmail.com>
2019-10-11 21:41:08 +0200

    fixing sintax errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,8 @@ def bubble_sort(array)
   (array.length-1).times do
     i = 0
     while i < (array.length-1)
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
       end
       i+= 1
     end
