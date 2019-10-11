5fa33ea69f96b332e48cfe26a21466f537f2c548
lhscar <gagc056@gmail.com>
2019-10-11 21:40:43 +0200

    fixing sintax errors

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,15 +1,15 @@
 def bubble_sort_by(array)
   if block_given?
-    (array.length-1).times do |i|
-      i=0
+    (array.length - 1).times do |i|
+      i = 0
       if yield(array[i], array[i + 1]).positive
         array[i], array[i + 1] = array[i + 1], array[i]
       end
-      i+=1
+      i +=1
     end
   else
     array
   end
 end
 x = ["abcd", "ab", "abcde", "abc", "a"]
-puts bubble_sort_by(x) { |left,right| left.length - right.length }
+puts bubble_sort_by(x) { |left, right| left.length - right.length }
